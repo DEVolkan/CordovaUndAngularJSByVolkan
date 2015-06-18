89eec8e8d72c9c1f8c6743cf009027dd87b2d02a
Volkan-Lord-Of-Fire <volkan.t.simsek@outlook.com>
2015-06-18 12:55:04 +0200

    Sqlite versucht einzubinden

--- CordovaUndAngularJS/platforms/android/assets/www/scripts/run.js
+++ CordovaUndAngularJS/platforms/android/assets/www/scripts/run.js
@@ -1,6 +1,7 @@
-(function(angular) {
+var db = null;
+(function (angular) {
     angular.module('app')
-            .run(["$location", "service", function ($location, service) {
+            .run(["$location", "service", "$cordovaSQLite", function ($location, service, $cordovaSQLite) {
                 function cordova () {
                     "use strict";
 
@@ -10,7 +11,10 @@
                         // Verarbeiten der Cordova-Pause- und -Fortsetzenereignisse
                         document.addEventListener( 'pause', onPause.bind( this ), false );
                         document.addEventListener('resume', onResume.bind(this), false);
-        
+
+                        console.log($cordovaSQLite);
+                        //db = $cordovaSQLite.openDB("my.db");
+                        //$cordovaSQLite.execute(db, "CREATE TABLE IF NOT EXISTS people (id integer primary key, firstname text, lastname text)");
                         // TODO: Cordova wurde geladen. Führen Sie hier eine Initialisierung aus, die Cordova erfordert.
                     };
 
