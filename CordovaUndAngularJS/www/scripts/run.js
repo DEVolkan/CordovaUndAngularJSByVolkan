c2eec4366c5214b5fde4f11118883afac68106c2
Volkan-Lord-Of-Fire <volkan.t.simsek@outlook.com>
2015-06-19 11:04:15 +0200

    sqlitePlugin still doesn't work

--- CordovaUndAngularJS/www/scripts/run.js
+++ CordovaUndAngularJS/www/scripts/run.js
@@ -2,19 +2,24 @@ var db = null;
 (function (angular) {
     angular.module('app')
             .run(["$location", "service", "$cordovaSQLite", function ($location, service, $cordovaSQLite) {
-                function cordova () {
+                function cordova() {
                     "use strict";
 
-                    document.addEventListener( 'deviceready', onDeviceReady.bind( this ), false );
+                    document.addEventListener('deviceready', onDeviceReady.bind(this), false);
 
                     function onDeviceReady() {
                         // Verarbeiten der Cordova-Pause- und -Fortsetzenereignisse
-                        document.addEventListener( 'pause', onPause.bind( this ), false );
+                        document.addEventListener('pause', onPause.bind(this), false);
                         document.addEventListener('resume', onResume.bind(this), false);
 
-                        console.log($cordovaSQLite);
-                        //db = $cordovaSQLite.openDB("my.db");
-                        //$cordovaSQLite.execute(db, "CREATE TABLE IF NOT EXISTS people (id integer primary key, firstname text, lastname text)");
+                        window.sqlitePlugin.openDatabase({ name: 'hello-world.db' }, function (db) {
+                            db.executeSql("select length('tenletters') as stringlength", [], function (res) {
+                                var stringlength = res.rows.item(0).stringlength;
+                                console.log('got stringlength: ' + stringlength);
+                                document.getElementById('deviceready').querySelector('.received').innerHTML = 'stringlength: ' + stringlength;
+                            });
+                        });
+
                         // TODO: Cordova wurde geladen. Führen Sie hier eine Initialisierung aus, die Cordova erfordert.
                     };
 
@@ -30,5 +35,5 @@ var db = null;
                 }
                 cordova();
                 return service.loadData();
-                }]);
+            }]);
 })(angular);
