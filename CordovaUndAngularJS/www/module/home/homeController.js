de96b84ec0cd62339f67b260074fa58a1f5b9e8a
Volkan-Lord-Of-Fire <volkan.t.simsek@outlook.com>
2015-08-03 16:48:10 +0200

    Beispiele Konkretesiert

--- CordovaUndAngularJS/www/module/home/homeController.js
+++ CordovaUndAngularJS/www/module/home/homeController.js
@@ -2,26 +2,56 @@
     angular.module("app")
             .controller('homeController', homeController);
 
-    homeController.$inject = ['$scope', 'service'];
+    homeController.$inject = ['$scope', 'service', '$cordovaSQLite'];
 
-    function homeController($scope, service) {
+    function homeController($scope, service, $cordovaSQLite) {
         $scope.testIntegers = service.getTestIntegers;
-
+        console.log($cordovaSQLite);
         $scope.test = "HelloWorld";
         $scope.testDieZweite = ["1", "2"];
         $scope.output = "";
+        $scope.aufgabe1 = "Pakete abhollen";
+        $scope.aufgabe2 = "Unterlagen abgeben";
+        $scope.area1 = "DHL";
+        $scope.area2 = "Mich betreffend"
+        var db = null;
 
+
+        function createDB() {
+            var db = window.openDatabase("Database", "1.0", "TestDatabase", 200000);
+            db.transaction(executeQuery, errorCB, successCB);
+        };
+        //function createDB() {
+        //    var dbSize = 5 * 1024 * 1024; // 5mb initial database   
+        //    var db = window.openDatabase("Test Table", "", "TestTable", dbSize,
+        //    function () {
+        //        console.log('db successfully opened or created');
+        //    },
+        //    function () {
+        //        console.log('db error');
+        //    }
+        //    );
+        //    db.transaction(executeQuery, errorCB, successCB);
+        //};
+        $scope.ausfuehren = createDB;
 
         function executeQuery(tx) {
-            tx.executeSql('DROP TABLE IF EXISTS TestTable');
-            tx.executeSql('CREATE TABLE IF NOT EXISTS TestTable (id unique, data)');
-            tx.executeSql('INSERT INTO TestTable (id, data) VALUES (1, "あいうえお")');
-            tx.executeSql('INSERT INTO TestTable (id, data) VALUES (2, "かきくけこ")');
-            tx.executeSql('INSERT INTO TestTable (id, data) VALUES (3, "さしすせそ")');
-            tx.executeSql('INSERT INTO TestTable (id, data) VALUES (4, "たちつてと")');
-            tx.executeSql('INSERT INTO TestTable (id, data) VALUES (5, "なにぬねの")');
-            tx.executeSql('INSERT INTO TestTable (id, data) VALUES (6, "はひふへほ")');
-            tx.executeSql('INSERT INTO TestTable (id, data) VALUES (7, "まみむめも")');
+            //tx.executeSql('DROP TABLE IF EXISTS TestTable');
+            tx.executeSql('CREATE TABLE IF NOT EXISTS TestTable (id unique, task, area)');
+            //tx.executeSql('CREATE TABLE TestTable (id unique, task, area)');
+            tx.executeSql('INSERT INTO TestTable (id, task, area) VALUES (1, "' + $scope.aufgabe1 + '", "' + $scope.area1 + '")');
+            tx.executeSql('INSERT INTO TestTable (id, task, area) VALUES (2, "' + $scope.aufgabe2 + '", "' + $scope.area2 + '")');
+        };
+
+        $scope.ausgabe = successCB()
+
+        function errorCB(err) {
+            console.log("Error occured while executing SQL: " + err.code);
+        };
+
+        function successCB() {
+            var db = window.openDatabase("Database", "1.0", "TestDatabase", 200000);
+            db.transaction(queryDB, errorCB);
         };
 
         function queryDB(tx) {
@@ -36,22 +66,9 @@
             };
             $scope.output = result;
         };
-
-        function errorCB(err) {
-            console.log("Error occured while executing SQL: " + err.code);
-        };
-
-        function successCB() {
-            var db = window.openDatabase("Database", "1.0", "TestDatabase", 200000);
-            db.transaction(queryDB, errorCB);
-        };
-
-        function createDB() {
-            var db = window.openDatabase("Database", "1.0", "TestDatabase", 200000);
-            db.transaction(executeQuery, errorCB, successCB);
-
-        };
-        $scope.ausfuehren = createDB;
+        //function queryDB(tx) {
+        //    tx.executeSql('SELECT * FROM TestTable where area = "DHL"', [], querySuccess, errorCB);
+        //};
     };
 
 })(angular);
